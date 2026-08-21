44a923e46fee0f510d24a9e440e63eaf26b91212
Patrick <149443490+Hydra210@users.noreply.github.com>
2026-08-21 19:09:47 -0400

    Add new video track to tracks.js

--- tracks.js
+++ tracks.js
@@ -1,3 +1,4 @@
 const TRACKS = [
+  { url: "https://pub-a17495cad61f41da8d8e455e1292573b.r2.dev/YTDown.com_YouTube_I-KNOW-PEOPLE_Media_bEDEuyXk3gI_001_1080p.mp4", type: "video" },
   { url: "https://pub-a17495cad61f41da8d8e455e1292573b.r2.dev/YTDown.com_YouTube_A-BOY-IS-A-GUN_Media_9JQDPjpfiGw_001_1080p.mp4", type: "video" },
 ];
