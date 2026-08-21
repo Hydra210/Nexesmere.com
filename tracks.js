a14596065720ed8914755cafd0bd4ad96ece73ab
Patrick <149443490+Hydra210@users.noreply.github.com>
2026-08-21 17:05:51 -0400

    Update track URL in tracks.js

--- tracks.js
+++ tracks.js
@@ -1,3 +1,3 @@
 const TRACKS = [
-  { url: "https://pub-a17495cad61f41da8d8e455e1292573b.r2.dev/track1.mp4", type: "video" },
+  { url: "https://pub-a17495cad61f41da8d8e455e1292573b.r2.dev/YTDown.com_YouTube_A-BOY-IS-A-GUN_Media_9JQDPjpfiGw_001_1080p.mp4", type: "video" },
 ];
